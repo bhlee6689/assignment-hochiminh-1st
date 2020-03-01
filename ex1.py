e07a066aa7cc8a7f2bf8ae96154b33f1201123cf
bhlee6689 <61373206+bhlee6689@users.noreply.github.com>
2020-03-01 12:16:45 +0700

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,7 @@
+x = ['BHLEE', 1, True]
+print(x[1])
+
+y = ('bhlee', 2, False)
+print(y[0])
+print(y[1])
+print(y[2])
---
+++ ex2.py
@@ -0,0 +1,7 @@
+BHLEE = {'health': 95, 'power': 100, 'mana': 20, 'armor': 80, 'name': 'bhlee'}
+
+print(BHLEE['health'])
+print(BHLEE['power'])
+print(BHLEE['mana'])
+print(BHLEE['armor'])
+print(BHLEE['name'])
