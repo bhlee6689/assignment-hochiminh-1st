59ed3c5fcfd46213724f065830426ffc4182c46e
bhlee6689 <61373206+bhlee6689@users.noreply.github.com>
2020-03-02 08:10:42 +0700

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -1,12 +1,7 @@
-import requests
+x = ['BHLEE', 1, True]
+print(x[1])
 
-
-insta_API_URL = 'https://api.instagram.com/oembed?url=https://www.instagram.com/p/B8OBgnyjoFk/?utm_source=ig_web_copy_link'
-
-
-req = requests.get(insta_API_URL)
-print(req.url)
-
-res = req.json()['title']
-# location = res[0]['geometry']['location']
-print(res)
+y = ('bhlee', 2, False)
+print(y[0])
+print(y[1])
+print(y[2])
